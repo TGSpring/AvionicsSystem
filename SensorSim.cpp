355911449c7931d78769687739b0955f124a9507
TGSpring <TylerGSpring@gmail.com>
2024-09-04 14:11:51 -0400

    .

--- SensorSim.cpp
+++ SensorSim.cpp
@@ -1,7 +1,28 @@
 #include "SensorSim.h"
 #include <cstdlib> // for rand()
 
-SensorSim::SensorSim() : altitude(1000.0), airspeed(250.0), orientaion(0.0) {}
+// Custom clamp function
+template <typename T>
+T clamp(const T& value, const T& low, const T& high) {
+    return (value < low) ? low : (value > high) ? high : value;
+}
+
+// SensorSim Constructor
+SensorSim::SensorSim()
+    : altitude(1000.0), airspeed(250.0), orientation(0.0),
+    noise_stddev(1.0), temperature(15.0), humidity(50.0),
+    gen(rd()), noise_dist(0.0, noise_stddev) {}
+
+// Realistic Ranges for Sensors
+const double MAX_ALTITUDE = 50000.0;
+const double MIN_ALTITUDE = 0.0;
+const double MAX_AIRSPEED = 600.0;
+const double MIN_AIRSPEED = 0.0;
+
+// Simulated Effect Coefficients
+const double TEMP_EFFECT = 0.05;
+const double HUMIDITY_EFFECT = 0.02;
+
 
 double SensorSim::getAltitude() const {
 	return altitude;
@@ -18,7 +39,17 @@ double SensorSim::getOrientation() const {
 void SensorSim::updateSensor() {
 	//Simulate sensor udpates with random changes
 
-	altitude += (rand() % 20 - 10); //Random changes between -10 and 10
-	airspeed += (rand() % 10 - 5); //Random changes between -5 and 5
-	orientaion += (rand() % 6 - 3); //Random changes between -3 and 3
+    // Update altitude with Gaussian noise and temperature effect
+    double altitude_change = uniform_dist(gen) + noise_dist(gen) - TEMP_EFFECT * (temperature - 15.0);
+    altitude += altitude_change;
+    altitude = clamp(altitude, MIN_ALTITUDE, MAX_ALTITUDE);
+
+    // Update airspeed with Gaussian noise and humidity effect
+    double airspeed_change = uniform_dist(gen) + noise_dist(gen) - HUMIDITY_EFFECT * (humidity - 50.0);
+    airspeed += airspeed_change;
+    airspeed = clamp(airspeed, MIN_AIRSPEED, MAX_AIRSPEED);
+
+    // Update orientation with Gaussian noise
+    double orientation_change = uniform_dist(gen) + noise_dist(gen);
+    orientation += orientation_change;
 }
